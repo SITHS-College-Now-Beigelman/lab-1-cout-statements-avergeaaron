cda640f135d56151820000e5d33ab1818fb461a6
aaron lin <aaronl199@nycstudents.net>
2024-09-10 17:12:09 -0400

    Lab 1

--- Lin_Aaron_Lab_1.cpp
+++ Lin_Aaron_Lab_1.cpp
@@ -1,4 +1,4 @@
-// Aaron Lin 9/10/2024 //
+// Aaron Lin In-Class Lab 1: cout Statements 9/10/2024 //
 #include <iostream>
 using namespace std;
 
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     cout << "####################################################################################" << endl;
+    cout << "#                                                                                  #" << endl;
     cout << "#  Ways to access the Task Manager on your Windows computer:                       #" << endl;
     cout << "#                                                                                  #" << endl;
     cout << "#         Press the key combination Ctrl + Shift + Escape                          #" << endl;
@@ -18,3 +19,18 @@ int main()
 
     return 0;
 }
+
+/*[Running] cd "c:\Users\college\Documents\GitHub\lab-1-cout-statements-avergeaaron\" && g++ Lin_Aaron_Lab_1.cpp -o Lin_Aaron_Lab_1 && "c:\Users\college\Documents\GitHub\lab-1-cout-statements-avergeaaron\"Lin_Aaron_Lab_1
+####################################################################################
+#                                                                                  #
+#  Ways to access the Task Manager on your Windows computer:                       #
+#                                                                                  #
+#         Press the key combination Ctrl + Shift + Escape                          #
+#                                                                                  #
+#          Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+#                                                                                  #
+#          Type “Task Manager” in the Windows Start menu search                    #
+#                                                                                  #
+####################################################################################
+
+[Done] exited with code=0 in 1.449 seconds*/
