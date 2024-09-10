732533565722ce9e35e9a80df177c9fe09fc5d47
aaron lin <aaronl199@nycstudents.net>
2024-09-10 17:03:05 -0400

    Lab 1

---
+++ Lin_Aaron_Lab_1.cpp
@@ -0,0 +1,20 @@
+// Aaron Lin 9/10/2024 //
+#include <iostream>
+using namespace std;
+
+//beginning of code//
+int main()
+{
+    cout << "####################################################################################" << endl;
+    cout << "#  Ways to access the Task Manager on your Windows computer:                       #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#         Press the key combination Ctrl + Shift + Escape                          #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#          Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#          Type “Task Manager” in the Windows Start menu search                    #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "####################################################################################" << endl; 
+
+    return 0;
+}
